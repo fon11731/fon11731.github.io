a99243d9e65605f91c608299da874f8396018278
fon11731 <fon11731@163.com>
2025-12-23 23:03:14 +0800

    Site updated by  (v): YYYY年12月2025年12月23日日 23:03:14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1766502014540";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1766502191235";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
