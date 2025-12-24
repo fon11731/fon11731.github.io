dda59ea15f32d70e3eda3406d3ba88506588ee7e
fon11731 <fon11731@163.com>
2025-12-24 23:49:42 +0800

    网站已更新: YYYY-12-2025年12月24日 23:49:41

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1766588784461";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1766591377975";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
